3d7768de63824627e367e2128e0a1904ff644b34
teunmooij <tmooij@infinitaslearning.com>
2023-01-18 23:46:19 +0100

    still wip

--- src/component.ts
+++ src/component.ts
@@ -10,8 +10,8 @@ interface SignalFn {
 }
 
 interface AwaitFn {
-  <TExpected = any>(key: string, timeout?: number): Promise<TExpected>;
-  <TExpected = any>(timeout?: number): Promise<TExpected>;
+  <TExpected = any>(key: string, timeoutMs?: number): Promise<TExpected>;
+  <TExpected = any>(timeoutMs?: number): Promise<TExpected>;
 }
 
 export interface Converse {
@@ -25,32 +25,46 @@ export interface Config {
 
 interface SignalContext {
   timestamp: number;
-  data?: any;
-  waitPromise?: Promise<any>;
+  resolve: (data: any) => void;
+  resultPromise: Promise<any>;
 }
 
 const defaultKey = 'Betisman';
+const resolveKey = 'neodmy';
+const timeoutKey = 'hamsaaldrobi';
 
-const isResolved = async (promise: Promise<any>) => {
-  const test = {};
-  const first = await Promise.race([promise, test]);
-  return first !== test;
+const isResolved = async (promise: Promise<any>) => Promise.race([promise, resolveKey]).then(value => value !== resolveKey);
+
+const withTimeout = (promise: Promise<any>, timeoutMs: number | undefined) => {
+  if (!timeoutMs) return promise;
+  const sleep = new Promise(resolve => setTimeout(() => resolve(timeoutKey), timeoutMs));
+  return Promise.race([promise, sleep]);
 };
 
 export const initConverse = (): Component<Converse, { config?: Config }> => ({
   start: async ({ config: { maxSignals = 1000 } = {} }) => {
     const contexts: Record<string, SignalContext> = {};
 
-    const awaitfn: AwaitFn = async () => { }
-
     return {
-      signal: (...args) => {
-        const key = typeof args[0] === 'string' ? args[0] : defaultKey;
-        const options = typeof args[0] === 'object' ? args[0] : args[1];
-
-
+      signal: (arg0: string | SignalOptions, arg1?: SignalOptions) => {
+        const key = typeof arg0 === 'string' ? arg0 : defaultKey;
+        const options = typeof arg0 === 'object' ? arg0 : arg1;
       },
-      await: awaitfn,
+      await: (arg0: string | number, arg1?: number) => {
+        const key = typeof arg0 === 'string' ? arg0 : defaultKey;
+        const timeoutMs = typeof arg0 === 'number' ? arg0 : arg1;
+
+        const signalContext = contexts[key];
+        if (signalContext) return withTimeout(signalContext.resultPromise, timeoutMs);
+
+        const newContext = { timestamp: Date.now() } as SignalContext;
+        newContext.resultPromise = new Promise(resolve => {
+          newContext.resolve = resolve;
+        });
+        contexts[key] = newContext;
+
+        return withTimeout(newContext.resultPromise, timeoutMs);
+      },
     };
   },
 });
